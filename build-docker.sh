d4f419ffb9e072f649f1ac0dd105e1cdf6717a91
will <yh@fir.im>
2017-10-20 15:17:10 +0800

    change

--- build-docker.sh
+++ build-docker.sh
@@ -7,10 +7,14 @@
 # How to run flow.ci
 # - backend: 'docker-compose up'
 # - agent: docker run --network=host -e FLOW_ZOOKEEPER_HOST=127.0.0.1:2181 -e FLOW_AGENT_ZONE=default -e FLOW_AGENT_NAME={agent name} flow.ci.agent
-cd /Users/firim/workspace/flow-pf/flow-platform
+FLOW_PLATFORM_PATH=../flow-platform
+FLOW_WEB_PATH=../flow-web
+
+cd $FLOW_PLATFORM_PATH
 # mvn build artifact of each components
 mvn clean install -DskipTests=true
 
+cp ../docker/Dockerfile-backend .
 # build docker image for flow.ci backend
 docker build -t flow.ci.backend -f ./Dockerfile-backend .
 
@@ -18,12 +22,15 @@ docker build -t flow.ci.backend -f ./Dockerfile-backend .
 docker-compose rm -f
 docker-compose build
 
+
+cp ../docker/Dockerfile-agent .
 # build docker image for flow.ci agent
 docker build -t flow.ci.agent -f ./Dockerfile-agent .
 
-cd /Users/firim/workspace/flow-pf/flow-web
-
+# build web
+cd $FLOW_WEB_PATH
 FLOW_WEB_API=http://localhost:8080/flow-api   npm run  build
-DOCKER_PATH=/Users/firim/workspace/flow-pf/docker/
+
+cp ../flow-web/Dockerfile-web .
 docker build -t flow.web -f ./Dockerfile-web .
 # docker build -t will/tomcat-git -f ./Dockerfile-git .
