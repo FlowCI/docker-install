110c92cd030fb414605f8bcd79811d9d0328c8b0
will <yh@fir.im>
2017-11-06 14:52:28 +0800

    change code

--- start-services.sh
+++ start-services.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+echo "###########环境变量说明###########"
+echo "FLOW_API: 部署的FlowApi地址, 默认是 http://localhost:8080/flow-api"
+
+if [[ ! -n $FLOW_API ]]; then
+	FLOW_API=http://localhost:8080/flow-api
+fi
+
 docker-compose up
+
 
