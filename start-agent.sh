3f7dcc2eb8db9295001304e7e98a5c7eefd451e0
will <yh@fir.im>
2017-11-07 14:18:21 +0800

    修改code

--- start-agent.sh
+++ start-agent.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 # example ./start-agent.sh http://127.0.0.1:8080/flow-api cfc4a6f1-a2ea-4263-83b2-5f68eadaecbe
-docker run --network=host -e FLOW_ZOOKEEPER_HOST=127.0.0.1:2181 -e FLOW_BASE_URL=$1 -e FLOW_TOKEN=$2 flow.ci.agent
+
+FLOW_ZOOKEEPER_HOST=127.0.0.1:2181
+DOCKER_IMAGE_AGENT=flowci/flow.ci.agent
+
+docker run --network=host -e FLOW_ZOOKEEPER_HOST=$FLOW_ZOOKEEPER_HOST -v ~/.ssh:/root/.ssh -e FLOW_BASE_URL=$1 -e FLOW_TOKEN=$2 -d $DOCKER_IMAGE_AGENT
+
+echo "=============================start agent success============================="
