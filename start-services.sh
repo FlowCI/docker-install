dadaf83e1899838ded3d7cc4d1446202e11d6c74
will <yh@fir.im>
2017-11-09 19:21:00 +0800

    Xms Xmx 设置为环境变量

--- start-services.sh
+++ start-services.sh
@@ -4,6 +4,8 @@ echo "###########环境变量说明###########"
 
 export MYSQL_USER=root
 echo "MYSQL_USER: 配置的Mysql的初始用户名, 默认是 root , 不可修改"
+export JAVA_OPTS="-Xms1536m -Xmx1536m"
+echo "JVM 最大堆大小为 1.5G"
 
 if [[ ! -n $MYSQL_PASSWORD ]]; then
 	export MYSQL_PASSWORD=flow.ci
@@ -13,12 +15,12 @@ echo "MYSQL_PASSWORD: 配置的Mysql的初始密码, 默认是 flow.ci , 当前�
 if [[ ! -n $FLOW_API_DOMAIN ]]; then
 	export FLOW_API_DOMAIN=127.0.0.1
 fi
-echo "FLOW_API_DOMAIN: 部署的FlowApi地址, 默认是 localhost, 当前参数值是 ${FLOW_API_DOMAIN}"
+echo "FLOW_API_DOMAIN: 部署的FlowApi地址, 默认是 127.0.0.1, 当前参数值是 ${FLOW_API_DOMAIN}"
 
 if [[ ! -n $FLOW_WEB_DOMAIN ]]; then
 	export FLOW_WEB_DOMAIN=127.0.0.1
 fi
-echo "FLOW_WEB_DOMAIN: 部署的FlowWeb地址, 默认是 localhost, 当前参数值是 $FLOW_WEB_DOMAIN"
+echo "FLOW_WEB_DOMAIN: 部署的FlowWeb地址, 默认是 127.0.0.1, 当前参数值是 $FLOW_WEB_DOMAIN"
 
 if [[ ! -n $FLOW_SYS_EMAIL ]]; then
 	export FLOW_SYS_EMAIL=admin@flow.ci
