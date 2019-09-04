5137a729fd91c6de4cb131e8a0f3651483585a75
gy2006 <32008001@qq.com>
2019-09-04 16:48:05 +0200

    apply server host instead of url in agent

--- start-agent.sh
+++ start-agent.sh
@@ -6,11 +6,12 @@
 ### EXAMPLE: ./start-agent.sh http://172.20.10.4:8080 cfc4a6f1-a2ea-4263-83b2-5f68eadaecbe
 ### HINT: The "localhost" or "127.0.0.1" is NOT applicable for argument {server ip}
 
-FLOWCI_SERVER_URL=$1
+FLOWCI_SERVER_HOST=$1
+FLOWCI_SERVER_HOST=8080
 FLOWCI_AGENT_TOKEN=$2
 
-if [[ ! -n $FLOWCI_SERVER_URL ]]; then
-	echo "[ERROR] Server url is required"
+if [[ ! -n $FLOWCI_SERVER_HOST ]]; then
+	echo "[ERROR] Server host is required"
 	exit 1
 fi
 
@@ -20,7 +21,7 @@ if [[ ! -n $FLOWCI_AGENT_TOKEN ]]; then
 fi
 
 docker run \
--e FLOWCI_SERVER_URL=$FLOWCI_SERVER_URL \
+-e FLOWCI_SERVER_URL=http://$FLOWCI_SERVER_HOST:$FLOWCI_SERVER_HOST \
 -e FLOWCI_AGENT_TOKEN=$FLOWCI_AGENT_TOKEN \
 -v $HOME/.flow.ci.agent:/root/.flow.ci.agent \
 flowci/agent
