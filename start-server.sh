4fd5219f191a2318014053b84f7572cf9d7df225
Yang Guo <gy@fir.im>
2019-12-29 01:32:01 +0100

    add minio service

--- start-server.sh
+++ start-server.sh
@@ -16,11 +16,17 @@ export FLOWCI_ZOOKEEPER_HOST=$1
 export FLOWCI_DEFAULT_ADMIN_EMAIL=$2
 export FLOWCI_DEFAULT_ADMIN_PASSWORD=$3
 
+export FLOWCI_DEFAULT_MINIO_ACCESS_KEY=minio
+export FLOWCI_DEFAULT_MINIO_SECRET_KEY=minio123
+
 ## To define where to store the data of ci server in host
 export FLOWCI_SERVER_DIR=$HOME/.flow.ci
 export FLOWCI_SERVER_DB_DIR=$FLOWCI_SERVER_DIR/db
+export FLOWCI_SERVER_DATA_DIR=$FLOWCI_SERVER_DIR/data
+
 mkdir -p $FLOWCI_SERVER_DIR
 mkdir -p $FLOWCI_SERVER_DB_DIR
+mkdir -p $FLOWCI_SERVER_DATA_DIR
 
 if [[ ! -n $1 ]]; then
 	echo "[ERROR]: The host domain name or ip must be defined"
