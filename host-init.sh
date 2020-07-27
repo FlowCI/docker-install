a1f9f789a4401a62df19f98ba6c1f0fa0c748ebe
gy2006 <32008001@qq.com>
2020-07-27 16:58:14 +0200

    fix pull pyenv image before agent

--- host-init.sh
+++ host-init.sh
@@ -2,6 +2,7 @@
 
 ## the script to initialize ssh-host pool environment
 
-docker pull flowci/agent:latest
+docker pull flowci/pyenv:1.3
 docker volume create pyenv
 docker run --rm -v pyenv:/target flowci/pyenv:1.3 bash -c "/ws/init-pyenv-volume.sh"
+docker pull flowci/agent:latest
